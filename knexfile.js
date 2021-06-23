70253d240c3a3fd626f12c65f18eb728c3ca4603
Nilanjan Majumdar <n.majumder14@gmail.com>
2021-06-24 03:07:16 +0530

    restructured code

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
 // Update with your config settings.
+require("dotenv").config();
 
 module.exports = {client: "pg",
 connection: {
-  host: "localhost",
-  host: "127.0.0.1",
-  user: "postgres",
-  port:"5432",
-  password: "password",
-  database: "Intellect-intern",
+  host: process.env.db_host,
+  user: process.env.db_user,
+  port: process.env.db_port,
+  password: process.env.db_password,
+  database: process.env.db_database,
 },
   };
---
+++ src/database/index.js
@@ -0,0 +1,17 @@
+require("../dotenv").config();
+
+const knex = require("knex");
+const knexDb = knex( {client: "pg",
+connection: {
+    host: process.env.db_host,
+    user: process.env.db_user,
+    port: process.env.db_port,
+    password: process.env.db_password,
+    database: process.env.db_database,
+}} );
+const bookshelf = require("bookshelf");
+const securePassword = require("bookshelf-secure-password");
+const db = bookshelf(knexDb);
+db.plugin(securePassword);
+
+module.exports = db;
