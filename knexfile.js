9b12d5ce3815511d87545a42f83386dd54ce3370
Nilanjan Majumdar <n.majumder14@gmail.com>
2021-06-24 00:32:02 +0530

    basic structure implemented

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+// Update with your config settings.
+
+module.exports = {client: "pg",
+connection: {
+  host: "localhost",
+  host: "127.0.0.1",
+  user: "postgres",
+  port:"5432",
+  password: "password",
+  database: "Intellect-intern",
+},
+  };
